8db351b8149319c680fc5f4d144c4566ca325193
kijung <daiywlf5251@gmail.com>
2022-03-24 20:04:07 +0900

    add AppConfig
    AppConfig에서 어떤 구현체를 사용할지 정한다!

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { //application에 대한 환경설정(배우 섭외)는 여기서 다 한다.
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
