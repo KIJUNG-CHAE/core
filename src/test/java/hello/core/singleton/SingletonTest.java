9c58e7f67b31dd6f03fa78bc46304bfa3b1f156b
kijung <daiywlf5251@gmail.com>
2022-03-29 20:46:11 +0900

    add Singleton (Spring) & SingletonTest
    Notice stateful Singleton!!!

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +14,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
         //1. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
@@ -41,5 +43,22 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         //same == 객체 비교!
         //equal equals
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너 사용")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조 값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
