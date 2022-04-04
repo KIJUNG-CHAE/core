33b14c011c51ff4301abafb5c30ffc324aa910d1
kijung <daiywlf5251@gmail.com>
2022-04-04 21:23:18 +0900

    수동 빈 등록(@Bean) vs 자동 빈 등록(컴포넌트 스캔)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +14,10 @@ import org.springframework.stereotype.Component;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//    수동 > 자동 빈 등록
+//    하지만 스프링 부트는 디폴트로 오류를 발생하게끔 구현되어 있다.
 }
