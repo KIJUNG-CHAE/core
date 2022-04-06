12fc16f0e51a76edeabecf0f002b6ced77a849d0
kijung <daiywlf5251@gmail.com>
2022-04-06 16:57:44 +0900

    add Init, destory func (Bean attribute)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -25,18 +25,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     //서비스 종료시 호출
     public void disconnect() {
-        System.out.println("close" + url);
+        System.out.println("close: " + url);
     }
 
-    @Override //의존관계 주입이 끝나면 호출하는 함수
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
