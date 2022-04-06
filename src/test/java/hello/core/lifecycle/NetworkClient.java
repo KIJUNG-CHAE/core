aee1f95e2e3f508b0e289deeb65ed04720c1b1f4
kijung <daiywlf5251@gmail.com>
2022-04-06 17:03:09 +0900

    add Init,destroy func (Annotation)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -28,12 +31,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
-    public void init() throws Exception {
+    @PostConstruct
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
